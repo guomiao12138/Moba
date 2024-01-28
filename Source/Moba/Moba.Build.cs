ddb69ad5e5a851d10e3bf56b751b04b8535a08a2
guomiao12138 <gmiao12138@163.com>
2024-01-28 23:25:51 +0800

    1

--- Source/Moba/Moba.Build.cs
+++ Source/Moba/Moba.Build.cs
@@ -8,7 +8,13 @@ public class Moba : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { 
+			"Core", 
+			"CoreUObject",
+			"Engine", 
+			"InputCore",
+            "AIModule",
+        });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
