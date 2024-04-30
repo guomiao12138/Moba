e7d98d7f56678e665783a347c7ca68df2080fa5d
guomiao <gmiao12138@163.com>
2024-04-30 19:18:38 +0800

    1

--- Source/Moba/Socket/MobaSocketSubsystem.cpp
+++ Source/Moba/Socket/MobaSocketSubsystem.cpp
@@ -9,6 +9,7 @@
 #include "Sockets.h"
 #include "Interfaces/IPv4/IPv4Address.h"
 #include "Moba/Setting/SocketDeveloperSettings.h"
+#include "Engine/NetDriver.h"
 
 
 void UMobaSocketSubsystem::Initialize(FSubsystemCollectionBase& Collection)
@@ -17,6 +18,7 @@ void UMobaSocketSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 
 	CreateSocket();
 
+	//SeverSocket->Listen();
 	if (Socket.IsValid())
 	{
 		MobaSocketRunnable = MakeShared<FMobaSocketRunnable>(Socket);
@@ -39,6 +41,16 @@ void UMobaSocketSubsystem::Deinitialize()
 	{
 		MobaSocketRunnable.Reset();
 	}
+
+	if (auto driver = GetWorld()->GetNetDriver())
+	{
+		auto ClientConnections = driver->ClientConnections;
+		for (auto c : ClientConnections)
+		{
+			c->CleanUp();
+		}
+	}
+
 }
 
 void UMobaSocketSubsystem::CreateSocket()
@@ -90,6 +102,44 @@ FMobaSocketRunnable::FMobaSocketRunnable(TSharedPtr<FSocket>& InSocket)
 {
 	Socket = InSocket;
 	Thread = FRunnableThread::Create(this, TEXT("Bridge Background Service Thread"));
+
+	//server
+	//SeverSocket->Bind(*addr);
+	//SeverSocket->Listen(0);
+	bool canbindall;
+	TSharedPtr<FInternetAddr> ListenAddr = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->GetLocalHostAddr(*GLog, canbindall);
+	//ListenAddr->SetPort(Port);
+
+	// create a server TCP socket
+	SeverSocket = TSharedPtr<FSocket>(ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateSocket(NAME_Stream, TEXT("MobaSocketSubsystem-listen"), ListenAddr->GetProtocolType()));
+
+	if (!SeverSocket)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Could not create listen SeverSocket."));
+	}
+	else
+	{
+		Socket->SetReuseAddr();
+		Socket->SetNoDelay();
+
+		// bind to the address
+		if (!Socket->Bind(*ListenAddr))
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Failed to bind listen socket %s in MobaSocketSubsystemServerTCP"), *ListenAddr->ToString(true));
+		}
+		// listen for connections
+		else if (!Socket->Listen(16))
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Failed to listen on socket %s in MobaSocketSubsystemServerTCP"), *ListenAddr->ToString(true));
+		}
+		else
+		{
+			// set the port on the listen address to be the same as the port on the socket
+			int32 port = Socket->GetPortNo();
+			//check((Port == 0 && port != 0) || port == Port);
+			ListenAddr->SetPort(port);
+		}
+	}
 }
 
 bool FMobaSocketRunnable::Init()
@@ -101,19 +151,42 @@ uint32 FMobaSocketRunnable::Run()
 {
 	while (IsRuning && Socket)
 	{
-		uint8 Data[512];
+		uint32 size;
 		int32 ResultID = 0;
 		int32 BytesRead;
 		TArray<uint8> content;
-		if (Socket->Recv(content.GetData(), sizeof(int32), BytesRead))
+		if (Socket->HasPendingData(size))
 		{
-			const std::string cstr(reinterpret_cast<const char*>(content.GetData()), content.Num());
-			FString frameAsFString = cstr.c_str();
-			UE_LOG(LogTemp, Warning, TEXT("Data %s"), *frameAsFString);
-			//FLocalReceivedPacket InCommingpacket;
-			//DispatchPacket();
+			content.SetNum(size);
+			if (Socket->Recv(content.GetData(), sizeof(int32), BytesRead))
+			{
+				const std::string cstr(reinterpret_cast<const char*>(content.GetData()), content.Num());
+				FString frameAsFString = cstr.c_str();
+				UE_LOG(LogTemp, Warning, TEXT("Data %s"), *frameAsFString);
+				//FLocalReceivedPacket InCommingpacket;
+				//DispatchPacket();
+				int32 sent = 0;
+				FString serialized = TEXT("aaaaaa");
+				TCHAR* serializedChar = serialized.GetCharArray().GetData();
+				Socket->Send((uint8*)TCHAR_TO_UTF8(serializedChar), serialized.Len(), sent);
+			}
 		}
+
+		if (SeverSocket)
+		{
+			uint32 size1;
+			if (SeverSocket->HasPendingData(size1))
+			{
+				FString des;
+				TSharedRef<FInternetAddr> addr = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateInternetAddr();
+				SeverSocket->Accept(*addr, *des);
+				UE_LOG(LogTemp, Warning, TEXT("Data %s"), *des);
+			}
+		}
+
 	}
+
+
 
 	return uint32();
 }
@@ -143,6 +216,10 @@ FMobaSocketRunnable::~FMobaSocketRunnable()
 		Thread->Kill();
 		delete Thread;
 		Thread = nullptr;
+	}
+	if (SeverSocket)
+	{
+		SeverSocket.Reset();
 	}
 }
 
--- Source/Moba/Socket/MobaSocketSubsystem.h
+++ Source/Moba/Socket/MobaSocketSubsystem.h
@@ -44,44 +44,17 @@ public:
 	TSharedPtr<FSocket> Socket;
 	FRunnableThread* Thread = nullptr;
 	TAtomic<bool> IsRuning = true;
+
+	TSharedPtr<FSocket> SeverSocket;
+
 public:
 
 	FMobaSocketRunnable(TSharedPtr<FSocket>& InSocket);
 
 	virtual bool Init() override;
-	/**
-	 * Runs the runnable object.
-	 *
-	 * This is where all per object thread work is done. This is only called if the initialization was successful.
-	 *
-	 * @return The exit code of the runnable object
-	 * @see Init, Stop, Exit
-	 */
 	virtual uint32 Run() override;
-
-	/**
-	 * Stops the runnable object.
-	 *
-	 * This is called if a thread is requested to terminate early.
-	 * @see Init, Run, Exit
-	 */
 	virtual void Stop() override;
-
-	/**
-	 * Exits the runnable object.
-	 *
-	 * Called in the context of the aggregating thread to perform any cleanup.
-	 * @see Init, Run, Stop
-	 */
 	virtual void Exit() override;
-
-	/**
-	 * Gets single thread interface pointer used for ticking this runnable when multi-threading is disabled.
-	 * If the interface is not implemented, this runnable will not be ticked when FPlatformProcess::SupportsMultithreading() is false.
-	 *
-	 * @return Pointer to the single thread interface or nullptr if not implemented.
-	 */
-
 	virtual void Tick() override;
 	virtual class FSingleThreadRunnable* GetSingleThreadInterface()
 	{
